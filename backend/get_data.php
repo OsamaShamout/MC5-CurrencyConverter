4388cf65dfd0cc75f7f8768fe0a0bd971b56eed2
fsociety <osama.shamout@lau.edu>
2022-03-30 17:00:53 +0300

    Completed parsing and extracting info from lira.org website.

--- backend/get_data.php
+++ backend/get_data.php
@@ -6,8 +6,8 @@
 include ("db_info.php");
 
 //Obtain values from frontend
-$rate_lbp_buy = $_POST["rate_lbp_buy"];
-$rate_lbp_sell = $_POST["rate_lbp_sell"];
+$rate_lbp_buy = $_POST["buy"];
+$rate_lbp_sell = $_POST["sell"];
 
 //Prepare query to insert into DB "?" to avoid SQL injections
 $query = $mysqli->prepare("INSERT INTO lira_rates (rate_lbp_buy, rate_lbp_sell) VALUES (?, ?);");
@@ -18,12 +18,5 @@ $query->bind_param("ss", $rate_lbp_buy, $rate_lbp_sell);
 //Perform query
 $query->execute();
 
-$response = [];
-
-$response["Status"] = "Mabrouk!";
-
-$json_response = json_encode($response);
-
-echo $json_response;
 
 ?>
--- frontend/app/src/main/java/com/example/currencylive/MainActivity2.java
+++ frontend/app/src/main/java/com/example/currencylive/MainActivity2.java
@@ -1,32 +1,44 @@
 package com.example.currencylive;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.text.DecimalFormat;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity2 extends AppCompatActivity {
 
-
+    private String value_sell;
+    private String value_buy;
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
         protected String doInBackground(String... urls){
 
             //String to store API result.
-            String result = "";
 
             //Variables to initiate connection.
             URL url;
@@ -41,40 +53,65 @@ public class MainActivity2 extends AppCompatActivity {
                 InputStream in = https.getInputStream();
 
                 //Perform conversion function to obtain result as a string.
-                result = convertStreamToString(in);
+                String newLine = System.getProperty("line.separator");
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(in));
+                StringBuilder result = new StringBuilder();
+                for (String line; (line = reader.readLine()) != null; ) {
+                    if (result.length() > 0) {
+                        result.append(newLine);
+                    }
+                    result.append(line);
+                }
+                return result.toString();
             }
             catch(Exception e){
                 e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Error in receiving data.", Toast.LENGTH_LONG).show();
+
                 return null;
             }
 
-            return null;
         }
 
         protected void onPostExecute(String s){
+            super.onPostExecute(s);
 
-        }
+            try{
 
+                //Convert JSON objects into Strings.
+                JSONObject json_obj = new JSONObject(s);
+                String rate_sell = json_obj.getString("sell");
+                String rate_buy = json_obj.getString("buy");
 
-        //Function that converts stream from InputStream into a String
-        //Responsible for turning url text into a string.
-        protected String convertStreamToString(InputStream is) {
-            try {
-                ByteArrayOutputStream result = new ByteArrayOutputStream();
-                byte[] buffer = new byte[1024];
-                for (int length; (length = is.read(buffer)) != -1; ) {
-                    result.write(buffer, 0, length);
+                //Prepare the regex expression to extract the conversion rate
+                Pattern p = Pattern.compile("1648\\d\\d\\d\\d\\d\\d\\d\\d\\d,(\\d\\d\\d\\d\\d)", Pattern.MULTILINE);
+                Matcher m1 = p.matcher(rate_sell);
+                Matcher m2 = p.matcher(rate_buy);
+
+                //Create an array list and append to sell ArrayList upon finding a match into list for sell rate
+                ArrayList<String> arr = new ArrayList<String>();
+                while(m1.find()  == true){
+                    arr.add(m1.group(1));
                 }
+                Log.i("ArrayList Sell: ", Arrays.toString(arr.toArray()));
+                int need_index =  (arr.size()-3);
+                value_sell = arr.get(need_index).toString();
 
-                return result.toString();
+                //Create an array list and append to buy ArrayList upon finding a match into list for buy rate
+                ArrayList<String> arr2 = new ArrayList<String>();
+                while(m2.find()  == true){
+                    arr.add(m2.group(1));
+                }
+                Log.i("ArrayList Buy: ", Arrays.toString(arr.toArray()));
+                int need_index2 =  (arr.size()-3);
+                value_buy = arr.get(need_index2).toString();
             }
-            catch(IOException e){
-                e.printStackTrace();
-                return null;
-            }
+
+
             catch(Exception e){
                 e.printStackTrace();
-                return null;
+                Toast.makeText(getApplicationContext(), "Error in post execution.", Toast.LENGTH_LONG).show();
             }
         }
     }
@@ -86,11 +123,23 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        String url = "https://mcprojs.000webhostapp.com/backend/test.php";
+
+        String url = "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233013";
+
+        String url2 = "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233013";
 
         DownloadTask task = new DownloadTask();
 
         task.execute(url);
+       // task.execute(url2);
+
+        value_received = (TextView) findViewById(R.id.resultValue);
+
+
+
+
+
+
 
     }
 
@@ -100,8 +149,6 @@ public class MainActivity2 extends AppCompatActivity {
         ImageView country_flag_to = (ImageView) findViewById(R.id.coinIconTo);
 
         country_flag_to.setVisibility(View.GONE);
-        country_flag_to.setVisibility(0);
-
 
         if (flag_USA) {
             flag_USA = false;
