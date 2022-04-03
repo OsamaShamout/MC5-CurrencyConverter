11e57c509b32468043f925f6d384a512b9473628
fsociety <osama.shamout@lau.edu>
2022-04-03 18:38:16 +0300

    Modification to save progress.

--- frontend/app/src/main/java/com/example/currencylive/MainActivity2.java
+++ frontend/app/src/main/java/com/example/currencylive/MainActivity2.java
@@ -135,64 +135,7 @@ public class MainActivity2 extends AppCompatActivity {
             }
         }
     }
-    public class DataPackager {
 
-        String name, position, team;
-
-        /*
-        SECTION 1.RECEIVE ALL DATA WE WANNA SEND
-         */
-        public DataPackager(String name, String position, String team) {
-            this.name = name;
-            this.position = position;
-            this.team = team;
-        }
-
-        /*
-       SECTION 2
-       1.PACK THEM INTO A JSON OBJECT
-       1. READ ALL THIS DATA AND ENCODE IT INTO A FROMAT THAT CAN BE SENT VIA NETWORK
-        */
-        public String packData() {
-            JSONObject jo = new JSONObject();
-            StringBuffer packedData = new StringBuffer();
-
-            try {
-                jo.put("Name", name);
-                jo.put("Position", position);
-                jo.put("Team", team);
-
-                Boolean firstValue = true;
-
-                Iterator it = jo.keys();
-
-                do {
-                    String key = it.next().toString();
-                    String value = jo.get(key).toString();
-
-                    if (firstValue) {
-                        firstValue = false;
-                    } else {
-                        packedData.append("&");
-                    }
-
-                    packedData.append(URLEncoder.encode(key, "UTF-8"));
-                    packedData.append("=");
-                    packedData.append(URLEncoder.encode(value, "UTF-8"));
-
-                } while (it.hasNext());
-
-                return packedData.toString();
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-    }
     //Send data to DB.
     String value_user;
     String result_db1;
@@ -221,10 +164,16 @@ public class MainActivity2 extends AppCompatActivity {
                 StringBuffer packedData=new StringBuffer();
                 value_user = value_inputted.getText().toString();
 
-                String trial = "2392439234";
-                jo.put("buy", trial);
-                jo.put("sell", trial);
-                jo.put("currency", trial);
+                if(!flag_USA){
+                    currency = "LBP";
+                }
+
+                String rate_buyy = "244400";
+                String rate_selll = "244400";
+
+                jo.put("buy", rate_buyy);
+                jo.put("sell", rate_selll);
+                jo.put("currency", currency);
                 jo.put("amount", value_user);
 
                 Boolean firstValue=true;
@@ -249,6 +198,7 @@ public class MainActivity2 extends AppCompatActivity {
 
                 }while (it.hasNext());
 
+                Log.e("Pack data:", packedData.toString());
 
                 OutputStream os=conn.getOutputStream();
                 BufferedWriter wr=new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
@@ -384,8 +334,8 @@ public class MainActivity2 extends AppCompatActivity {
         String url1 = "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t20224316";
 
         //Perform obtaining buy and sell rate.
-       // CallLiraAPI task1 = new CallLiraAPI();
-       // task1.execute(url1);
+        // CallLiraAPI task1 = new CallLiraAPI();
+        // task1.execute(url1);
 
     }
 
